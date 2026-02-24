1273d9680dc223e6b9023827aeb5c7bc07dd51b2
Tegar Satria Kirana <143944553+tegark524@users.noreply.github.com>
2026-02-24 11:00:53 +0700

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios' // <-- Tambahkan ini
 
 import App from './App.vue'
 import router from './router'
+
+// Atur Base URL mengarah ke server aaPanel kamu
+axios.defaults.baseURL = 'https://api-brijisent.duckdns.org/api'
+// Wajibkan balasan berupa JSON
+axios.defaults.headers.common['Accept'] = 'application/json'
 
 const app = createApp(App)
 
