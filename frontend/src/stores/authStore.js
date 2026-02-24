11bbbbbdc5c8d4b972208574aed2a59f717a5367
Tegar Satria Kirana <143944553+tegark524@users.noreply.github.com>
2026-02-24 11:23:08 +0700

    Update authStore.js

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -9,7 +9,7 @@ export const useAuthStore = defineStore('auth', {
         // 1. Fungsi Login sekarang menerima EMAIL dan PASSWORD
         async login(email, password) {
             try {
-                const response = await axios.post(/login', {
+                const response = await axios.post('/login', {
                     email: email,
                     password: password
                 })
